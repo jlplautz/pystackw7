8cfbadbb0f1cd66ca265f044c95db55805beffcd
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-07-04 13:18:25 -0300

    build(issue-3): Created project and app perfil. (#4)

--- core/perfil/admin.py
+++ core/perfil/admin.py
@@ -1,3 +1,6 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Conta
+
+admin.site.register(Conta)
+admin.site.register(Categoria)
